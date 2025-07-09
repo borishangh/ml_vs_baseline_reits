53033aa5c995bdb6ccfd039220733b6432d7558b
delhizine <delhizine@gmail.com>
2025-07-09 17:00:42 +0530

    getstockdata fix

--- utils/ticker_data.py
+++ utils/ticker_data.py
@@ -5,5 +5,11 @@ from datetime import datetime, timedelta
 def get_stock_data(ticker, start=None, end=datetime.today()):
     if start is None:
         start = end - timedelta(days=365)
-    data = yf.download(ticker, start=start.strftime('%Y-%m-%d'), end=end.strftime('%Y-%m-%d'))
-    return data
+    df = yf.download(ticker, start=start.strftime('%Y-%m-%d'), end=end.strftime('%Y-%m-%d'))
+    
+    df.columns = df.columns.get_level_values(0)
+    df = df.reset_index()
+    df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    df.columns.name = None
+
+    return df
