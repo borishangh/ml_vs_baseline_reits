ba1c96b0c72a6a0afb4f06e6736809276f3fc1d1
delhizine <delhizine@gmail.com>
2025-07-10 02:02:23 +0530

    windoweddfs

--- utils/ticker_data.py
+++ utils/ticker_data.py
@@ -18,3 +18,15 @@ def get_stock_data(ticker, start=None, end=None):
     df.columns.name = None
     
     return df
+
+def windowed_dfs(df, size):
+    df = df[["Date", "Close"]]
+    
+    labels = np.array([f'{i}-days-before' for i in range(size-1, 0, -1)])
+    prices = np.array([df.Close.iloc[i:i+size-1] for i in range(len(df)-size)])
+    X = pd.DataFrame(prices, columns=labels)
+    
+    y = pd.DataFrame(df.iloc[size:]).reset_index()
+    y = y[['Date', 'Close']]
+    
+    return X,y
